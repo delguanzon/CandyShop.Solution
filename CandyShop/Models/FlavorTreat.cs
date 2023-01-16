82ef10c1350ad8ae45a54f4e6109295dbeee75fc
delguanzon <yodelguanzon@gmail.com>
2023-01-15 19:46:42 -0800

    Add Models for Treats,Flavors, FlavorTreats

---
+++ CandyShop/Models/FlavorTreat.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Models
+{
+    public class FlavorTreat
+    {
+        public int FlavorTreatId { get; set; }
+        public int FlavorId { get; set;} 
+        public int TreatId { get; set; }
+        public Flavor Flavor { get; set; }
+        public Treat Treat { get; set; }
+        
+    }
+}
---
+++ CandyShop/Models/Treat.cs
@@ -0,0 +1,13 @@
+namespace CandyShop.Models
+{
+  public class Treat
+  {
+    // properties, constructors, methods, etc. go here
+    
+    public int TreatId { get; set; }
+    public string Description { get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+
+  }
+}
