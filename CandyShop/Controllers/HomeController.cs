249de43a1056c514201eb64cfbbc223515aede34
delguanzon <yodelguanzon@gmail.com>
2023-01-16 02:08:50 -0800

    Update Homecontroller

--- CandyShop/Controllers/HomeController.cs
+++ CandyShop/Controllers/HomeController.cs
@@ -1,14 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using CandyShop.ViewModels;
+using CandyShop.Models;
+using System.Linq;
 
 namespace CandyShop.Controllers
 {
     public class HomeController : Controller
     {
 
+      private readonly CandyShopContext _db;
+
+      public HomeController(CandyShopContext db)
+      {
+        _db = db;
+      }
+
       [HttpGet("/")]
       public ActionResult Index()
       {
-        return View();
+        ListAllViewModel model = new ListAllViewModel();
+        model.Flavors = _db.Flavors.ToList();
+        model.Treats = _db.Treats.ToList();
+        return View(model);
       }
 
     }
