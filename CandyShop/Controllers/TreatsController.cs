e3bc9313f7df3a5726618e2bca5ad42c21b0e549
delguanzon <yodelguanzon@gmail.com>
2023-01-15 23:03:50 -0800

    Add MVC for Treats

---
+++ CandyShop/Controllers/TreatsController.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CandyShop.Models;
+
+namespace CandyShop.Controllers
+{
+    public class TreatsController: Controller
+    {
+        private readonly CandyShopContext _db;
+        public TreatsController(CandyShopContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Treat> model = _db.Treats.ToList();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Treat treat)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(treat);
+            }
+            {
+                _db.Treats.Add(treat);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
+        }
+
+        public ActionResult Details(int id)
+        {
+            Treat thisTreat = _db.Treats
+                .Include(treat => treat.JoinEntities)
+                .ThenInclude(join => join.Treat)
+                .FirstOrDefault(treat => treat.TreatId == id);
+            return View(thisTreat);
+        }
+        public ActionResult Edit(int id)
+        {
+            Treat thisTreat = _db.Treats
+                .Include(treat => treat.JoinEntities)
+                .ThenInclude(join => join.Treat)
+                .FirstOrDefault(treat => treat.TreatId == id);
+            return View(thisTreat);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Treat treat)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(treat);
+            }
+            else 
+            {
+                _db.Treats.Update(treat);
+                _db.SaveChanges();
+                return RedirectToAction("Details", new { id = treat.TreatId });
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            return View(thisTreat);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            _db.Treats.Remove(thisTreat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
